3d01304d4e975a5ea319c2f2c5822e523d06f96a
blibotics <blibotics@gmail.com>
2014-12-01 13:05:08 -0500

    Formatting and starting exercise 2

--- exercise_01/ex1.py
+++ exercise_01/ex1.py
@@ -6,16 +6,18 @@ print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 
-# STUDY DRILLS
+# ================================
+# ========= STUDY DRILLS =========
+# ================================
 
-## Make your script print another line
+#1  Make your script print another line
 print "Adding new line"
 
-## Make your script print only one of the lines.
+#2 Make your script print only one of the lines.
 ### Two options:
 ### 1 - Delete all lines except for line 1, in terminal run "python ex1.py"
 ### 2 - Comment out all lines except for line 1, in terminal run "python ex1.py" 
 
 
-## Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+#3 Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
 ### Making comments are fun! 
